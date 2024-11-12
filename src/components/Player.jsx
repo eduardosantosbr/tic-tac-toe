3dbde48f20368d978406bc3617f1b9b8f891d153
Eduardo Nunes Santos <eduardo.ns.nunes@gmail.com>
2024-11-11 21:33:42 -0300

    feat: Fisrt commit tic tac toe game

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player-info">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
+}
